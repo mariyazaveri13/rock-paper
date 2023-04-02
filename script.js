f57b7e65a84bf74b3be1a3226eb01dc0f5e390ca
Mariya Zaveri <mariyazaver13@gmail.com>
2023-04-02 12:37:14 +0530

    finished the logic for 5 times play

--- script.js
+++ script.js
@@ -16,34 +16,63 @@ function getComputerChoice(){
     return choice;
 }
 
-const playerChoice = prompt("Please enter your choice : Rock / paper / scissors");
-const computerChoice = getComputerChoice();
-
-console.log(playRound(playerChoice,computerChoice));
-
-function playRound(playerChoice,computerChoice){
+function playRound(playerChoice,computerChoice,playerScore=0,compScore=0){
     let player = playerChoice.toLowerCase();
-
     if((player == 'rock' && computerChoice == 'paper')
         || (player == 'paper' && computerChoice == 'scissors')
         ||  (player == 'scissors' && computerChoice == 'rock')
     ){
-        return `You Lose! ${computerChoice} beats ${player}`;
+        compScore++;
+        return [`You Lose! ${computerChoice} beats ${player}`,playerScore,compScore];
     }
 
     else if((computerChoice == 'rock' && player == 'paper')
     || (computerChoice == 'paper' && player == 'scissors')
     ||  (computerChoice == 'scissors' && player == 'rock')
     ){
-        return `You Win! ${player} beats ${computerChoice}`;
+        playerScore++
+        return [`You Win! ${player} beats ${computerChoice}`,playerScore,compScore];
     }
 
     else if(computerChoice == player){
-        return `It's a tie`;
+        return [`It's a tie`,playerScore,compScore];
     }
 
     else{
-        return `Please enter valid choice`;
+        return [`Please enter valid choice`,playerScore,compScore];
     }
-
 }
+
+function game(playerScore,compScore){
+    let game;
+
+    const playerChoice = prompt("Please enter your choice : Rock / paper / scissors");
+    const computerChoice = getComputerChoice();
+    game = playRound(playerChoice,computerChoice,playerScore,compScore);
+    console.log(game[0]);
+    
+    return [game[1],game[2]];
+}
+
+function startPlay(){
+    let playerScore =0;
+    let computerScore = 0;
+    for(let i = 0; i < 5;i++){
+        scores = game();
+        if(scores[0] > scores[1]){
+            playerScore++;
+        }
+        else if(scores[0] < scores[1]){
+            computerScore++;
+        }
+    }
+    if(playerScore>computerScore)
+        alert(`You won !! Congratulations!!!`);
+    else if(playerScore < computerScore)
+        alert(`Better luck next time`);
+    else 
+        alert(`It's a tie`);
+    
+}
+
+startPlay();
