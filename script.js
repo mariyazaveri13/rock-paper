5e34732f0dca9c306df8063501cb6a9d197d3e24
Mariya Zaveri <mariyazaver13@gmail.com>
2023-04-04 17:37:29 +0530

    added UI

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+var playerScore = 0;
+var compScore = 0;
+
 function getComputerChoice(){
     //this shall return max 0, 1, or 2
     let random = Math.floor(Math.random() * 3);
@@ -16,14 +20,14 @@ function getComputerChoice(){
     return choice;
 }
 
-function playRound(playerChoice,computerChoice,playerScore=0,compScore=0){
+function playRound(playerChoice,computerChoice){
     let player = playerChoice.toLowerCase();
     if((player == 'rock' && computerChoice == 'paper')
         || (player == 'paper' && computerChoice == 'scissors')
         ||  (player == 'scissors' && computerChoice == 'rock')
     ){
         compScore++;
-        return [`You Lose! ${computerChoice} beats ${player}`,playerScore,compScore];
+        return `You lost this round! ${computerChoice} beats ${player}`;
     }
 
     else if((computerChoice == 'rock' && player == 'paper')
@@ -31,48 +35,62 @@ function playRound(playerChoice,computerChoice,playerScore=0,compScore=0){
     ||  (computerChoice == 'scissors' && player == 'rock')
     ){
         playerScore++
-        return [`You Win! ${player} beats ${computerChoice}`,playerScore,compScore];
+        return `You won this round! ${player} beats ${computerChoice}`;
     }
 
     else if(computerChoice == player){
-        return [`It's a tie`,playerScore,compScore];
+        return `It's a tie`;
     }
 
     else{
-        return [`Please enter valid choice`,playerScore,compScore];
+        return `Please enter valid choice`;
     }
 }
 
-function game(playerScore,compScore){
-    let game;
-
-    const playerChoice = prompt("Please enter your choice : Rock / paper / scissors");
+function game(playerChoice){
+    // const playerChoice = prompt("Please enter your choice : Rock / paper / scissors");
     const computerChoice = getComputerChoice();
-    game = playRound(playerChoice,computerChoice,playerScore,compScore);
-    console.log(game[0]);
-    
-    return [game[1],game[2]];
+    return playRound(playerChoice,computerChoice);
 }
 
-function startPlay(){
-    let playerScore =0;
-    let computerScore = 0;
-    for(let i = 0; i < 5;i++){
-        scores = game();
-        if(scores[0] > scores[1]){
-            playerScore++;
-        }
-        else if(scores[0] < scores[1]){
-            computerScore++;
-        }
+const radios = document.getElementsByName('rpc');
+for(let i = 0; i<radios.length;i++){
+    radios[i].addEventListener('click',(e)=>{
+        playGame(e);
+    });
+}
+
+function playGame(e){
+    
+    let msg = game(e.target.value);
+    document.getElementById('user').innerText = playerScore;
+    document.getElementById('computer').innerText = compScore;
+    document.getElementById('msg').innerText = msg;
+
+    if(playerScore == 5){
+        document.getElementById('user').innerText = playerScore;
+        alert(`You've won!! Thanks for saving humanity. Play again ?`);
+        resetGame();
+        return;
     }
-    if(playerScore>computerScore)
-        alert(`You won !! Congratulations!!!`);
-    else if(playerScore < computerScore)
-        alert(`Better luck next time`);
-    else 
-        alert(`It's a tie`);
-    
+    else if(compScore == 5){
+        document.getElementById('computer').innerText = compScore;
+        alert(`Uh oh! You let the computer win :( Try again?`);
+        resetGame();
+        return;
+    }
 }
 
-startPlay();
+function resetGame(){
+    document.getElementById('user').innerText = 0;
+    document.getElementById('computer').innerText = 0;
+    document.getElementById('msg').innerText = ''; 
+    compScore = 0;
+    playerScore = 0;
+}
+
+window.addEventListener("keydown", function(e) {
+    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
